bb03fcdc11ba9d5013e0ded0aa9de80d507fd8cf
Kornuletz <132944959+KornyIsDeveloping@users.noreply.github.com>
2023-10-22 12:59:07 +0300

    extra large desktop added
    I've implemented responsivity for the extra large desktop view

--- blackjack/src/gameMenu/MainMenu.tsx
+++ blackjack/src/gameMenu/MainMenu.tsx
@@ -3,6 +3,7 @@ import { Button } from '@mui/material'
 import { styled } from '@mui/system'
 
 //I choosed the styled utility because is dynamic based on props and I found it more flexible  
+//styling the entire game container
 const GameMenuContainer = styled('div')({
    //phone view
   '@media(min-width: 576px) and (max-width: 768px)': {
@@ -32,9 +33,30 @@ const GameMenuContainer = styled('div')({
     justifyContent: 'center',
     height: '100vh',
     background: '#2d3e50'
+  },
+
+  //large desktop view
+  '@media (min-width: 1201px) and (max-width: 1400px)': {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    background: '#2d3e50'
+  },
+
+  //extra lerge desktop view
+  '@media (min-width: 1400px) and (max-width: 1900px)': {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    background: '#2d3e50'
   }
 });
 
+//styling the button container to display buttons inline on with > 768 
 const ButtonContainer = styled('div')({
   //phone view
   '@media(min-width: 576px) and (max-width: 768px)': {
@@ -48,18 +70,32 @@ const ButtonContainer = styled('div')({
   '@media(min-width: 769px) and (max-width: 993px)': {
     display: 'flex',
     justifyContent: 'center',
-    gap: '10px'
+    gap: '15px'
   },
 
   //desktop view
   '@media(min-width: 994px) and (max-width: 1200px)': {
     display: 'flex',
     justifyContent: 'center',
-    gap: '10px'
+    gap: '20px'
+  },
+
+  //large desktop view
+  '@media (min-width: 1201px) and (max-width: 1400px)': {
+    display: 'flex',
+    justifyContent: 'center',
+    gap: '25px'
+  },
+
+  //extra lerge desktop view
+  '@media (min-width: 1400px) and (max-width: 1900px)': {
+    display: 'flex',
+    justifyContent: 'center',
+    gap: '30px'
   }
-  
 });
 
+//styling buttons from menu
 const StyledMenuButtons = styled(Button)({
     //phone view
   '@media(min-width: 576px) and (max-width: 768px)': {
@@ -103,10 +139,40 @@ const StyledMenuButtons = styled(Button)({
       '&:hover': {
         borderColor: '#fde3a8',
       },
-  }
+  },
 
+  //large desktop view
+  '@media (min-width: 1201px) and (max-width: 1400px)': {
+    textTransform: 'none',
+    margin: '20px 0',
+    padding: '10px 30px',
+    width: '200px',
+    fontSize: '1.2rem',
+    textAlign: 'center',
+    color: '#fde3a8',
+    borderColor: '#fde3a8',
+      '&:hover': {
+        borderColor: '#fde3a8',
+      },
+  },
+
+  //extra lerge desktop view
+  '@media (min-width: 1400px) and (max-width: 1900px)': {
+    textTransform: 'none',
+    margin: '20px 0',
+    padding: '10px 30px',
+    width: '200px',
+    fontSize: '1.2rem',
+    textAlign: 'center',
+    color: '#fde3a8',
+    borderColor: '#fde3a8',
+      '&:hover': {
+        borderColor: '#fde3a8',
+      },
+  }
 });
 
+//styleing the h1 (Title)
 const GameTitle = styled('h1')({
   //phone view
   '@media(min-width: 576px) and (max-width: 768px)': {
@@ -125,12 +191,25 @@ const GameTitle = styled('h1')({
     fontSize: '7rem',
     color: '#fde3a8'
   },
+
+  //large desktop view
+  '@media (min-width: 1201px) and (max-width: 1400px)': {
+    fontSize: '8rem',
+    color: '#fde3a8'
+  },
+
+  //extra lerge desktop view
+  '@media (min-width: 1400px) and (max-width: 1900px)': {
+    fontSize: '9rem',
+    color: '#fde3a8'
+  }
 });
 
+//styleing the h2 (Subtitle)
 const GameSubtitle = styled('h2')({
   '@media(min-width: 576px) and (max-width: 768px)': {
     fontSize: '1.2rem',
-    marginBottom: '2rem',
+    marginBottom: '1rem',
     letterSpacing: '2px',
     color: '#fde3a8'
   },
@@ -138,7 +217,7 @@ const GameSubtitle = styled('h2')({
   //tablet view
   '@media(min-width: 769px) and (max-width: 993px)': {
     fontSize: '1.4rem',
-    marginBottom: '2rem',
+    marginBottom: '1rem',
     letterSpacing: '3px',
     color: '#fde3a8'
   },
@@ -146,12 +225,29 @@ const GameSubtitle = styled('h2')({
   //desktop view
   '@media(min-width: 994px) and (max-width: 1200px)': {
     fontSize: '1.6rem',
-    marginBottom: '2rem',
+    marginBottom: '1rem',
     letterSpacing: '4px',
     color: '#fde3a8'
   },
+
+  //large desktop view
+  '@media (min-width: 1201px) and (max-width: 1400px)': {
+    fontSize: '1.7rem',
+    marginBottom: '1rem',
+    letterSpacing: '5px',
+    color: '#fde3a8'
+  },
+
+  //extra lerge desktop view
+  '@media (min-width: 1400px) and (max-width: 1900px)': {
+    fontSize: '1.8rem',
+    marginBottom: '1rem',
+    letterSpacing: '6px',
+    color: '#fde3a8'
+  }
 });
 
+//highlighting certain letters for completing the design
 const HighlightedLetter = styled('span')({
   color: '#96281b'
 });
